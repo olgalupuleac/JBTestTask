96c8f8571f5b97bcde7c0f36be86284eed021ebb
Olga Lupuleac <olga.lupuleac@gmail.com>
2020-04-20 12:35:35 +0300

    Add colored output

--- Grep.cs
+++ Grep.cs
@@ -1,40 +1,90 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security;
 using NLog;
 using NLog.Targets;
 using NLog.Config;
 
 namespace JBTestTask
 {
+    /// <summary>
+    /// 
+    /// </summary> 
     public class Grep
     {
         private static readonly Logger Logger;
 
+        /// <summary>
+        /// Initializes logger.
+        /// </summary>
         static Grep()
         {
             var consoleTarget = new ColoredConsoleTarget();
             var config = new LoggingConfiguration();
-            config.AddRule(LogLevel.Info, LogLevel.Fatal, consoleTarget);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, consoleTarget);
             LogManager.Configuration = config;
             Logger = LogManager.GetCurrentClassLogger();
         }
 
+
+        /// <summary>
+        /// Checks if the line matches the pattern. By default,
+        /// returns true if a string contains the provided pattern. 
+        /// </summary>
+        /// <param name="line">is a line to be check</param>
+        /// <returns>true if the line matches</returns>
         public delegate bool MatcherDelegate(string line);
 
+        /// <summary>
+        /// Prints the resulting strings. 
+        /// </summary>
+        /// <param name="filename">Is a name of the file where the matching line was found</param>
+        /// <param name="index">Is a line number.</param>
+        /// <param name="line">Is the </param>
         public delegate void PrinterDelegate(string filename, int index, string line);
 
+        /// <summary>
+        /// 
+        /// </summary>
         public MatcherDelegate Matcher { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public PrinterDelegate Printer { get; set; }
 
-        public Grep(string pattern)
+        public Grep(string pattern, bool coloredOutput = true)
         {
             Matcher = line => line.Contains(pattern);
-            Printer = (filename, index, line) => Console.WriteLine($"{filename}:{index}: {line}");
+            if (coloredOutput)
+            {
+                Printer = (filename, index, line) =>
+                {
+                    int indexPattern = line.IndexOf(pattern, StringComparison.Ordinal);
+                    Debug.Assert(indexPattern != -1);
+                    var start = line.Substring(0, indexPattern);
+                    Logger.Debug($"{indexPattern + pattern.Length}, {line.Length}, {line}, {indexPattern}");
+                    var end = indexPattern + pattern.Length == line.Length
+                        ? ""
+                        : line.Substring(indexPattern + pattern.Length);
+                    ColoredConsole.PrintHighlighted($"{filename}:{index}: {start}", pattern, end);
+                };
+            }
+            else
+            {
+                Printer = (filename, index, line) => ColoredConsole.Print($"{filename}:{index}: {line}");
+            }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
         public void Execute(string path)
         {
+            Logger.Debug($"Current directory is {Directory.GetCurrentDirectory()}");
             if (File.Exists(path))
             {
                 ProcessFile(path);
@@ -43,25 +93,66 @@ namespace JBTestTask
 
             if (!Directory.Exists(path))
             {
-                Logger.Error($"{path} does not exist");
+                Logger.Warn($"{path} does not exist");
                 throw new ArgumentException($"The path {path} does not exist");
             }
 
             ProcessDirectory(path);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
         private void ProcessDirectory(string path)
         {
-            Logger.Info($"Processing directory {path}");
-            Directory.GetDirectories(path).ToList().ForEach(ProcessDirectory);
-            Directory.GetFiles(path).ToList().ForEach(ProcessFile);
+            Logger.Debug($"Processing directory {path}");
+            try
+            {
+                Directory.GetDirectories(path).ToList().ForEach(ProcessDirectory);
+                Directory.GetFiles(path).ToList().ForEach(ProcessFile);
+            }
+            catch (Exception exception)
+            {
+                Logger.Warn(exception.StackTrace);
+
+                if (exception is UnauthorizedAccessException || exception is PathTooLongException)
+                {
+                    ColoredConsole.ReportError(exception.Message);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filename"></param>
         private void ProcessFile(string filename)
         {
-            Logger.Info($"Processing file {filename}");
-            File.ReadLines(filename).Select((line, index) => (line, index)).Where(x => Matcher(x.line))
-                .ToList().ForEach(x => Printer(filename, x.index, x.line));
+            Logger.Debug($"Processing file {filename}");
+            try
+            {
+                File.ReadLines(filename).Select((line, index) => (line, index)).Where(x => Matcher(x.line))
+                    .ToList().ForEach(x => Printer(filename, x.index + 1, x.line));
+            }
+            catch (Exception exception)
+            {
+                Logger.Warn(exception.StackTrace);
+
+                if (exception is UnauthorizedAccessException || exception is PathTooLongException ||
+                    exception is SecurityException)
+                {
+                    ColoredConsole.ReportError(exception.Message);
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,81 @@
 ﻿using System;
+using NLog.Targets;
 
 namespace JBTestTask
 {
+    internal class ColoredConsole
+    {
+        private const ConsoleColor ErrorColor = ConsoleColor.Red;
+        private const ConsoleColor DefaultColor = ConsoleColor.Green;
+        private const ConsoleColor HighlightColor = ConsoleColor.Cyan;
+
+        static ColoredConsole()
+        {
+            Console.ForegroundColor = DefaultColor;
+        }
+
+        public static void ReportError(string message)
+        {
+            var normalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ErrorColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = normalColor;
+        }
+
+        public static void Print(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+        public static void PrintHighlighted(string start, string match, string end)
+        {
+            Console.Write(start);
+            Console.ForegroundColor = HighlightColor;
+            Console.Write(match);
+            Console.ForegroundColor = DefaultColor;
+            Console.WriteLine(end);
+        }
+    }
+
     public class Program
     {
+        
         public static void Main(string[] args)
         {
+            
             if (args.Length != 2)
             {
-                Console.WriteLine("Usage: <pattern> <path>");
+                ColoredConsole.ReportError("Usage: <pattern> <path>");
                 return;
             }
 
+            var pattern = args[0];
+            var path = args[1];
+
+
             try
             {
-                var grep = new Grep(args[0]);
+                var grep = new Grep(pattern)
+                {
+                    /*Printer = (filename, index, line) =>
+                    {
+                        Console.Write($"{filename}:{index}: ");
+                        var match = line.IndexOf(pattern, StringComparison.Ordinal);
+                        Console.Write(line.Substring(0, match + 1));
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write(pattern);
+                        Console.ForegroundColor = DefaultTextColor;
+                        Console.WriteLine(line.Substring(match + pattern.Length + 1));
+                    }*/
+                };
                 grep.Execute(args[1]);
-                Console.WriteLine();
             }
             catch (Exception exception)
             {
                 Console.WriteLine(exception.Message);
             }
+
+            Console.WriteLine();
         }
     }
 }
